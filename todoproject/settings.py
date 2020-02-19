1e854c3e49d85a3e41d89ad1fb67cc882c005fca
robbertdejong88 <55914370+robbertdejong88@users.noreply.github.com>
2020-02-19 16:15:40 +0100

    add heroku files and settings

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(71-5(c68khb+_0me#c7hnbge#@t1@#czrf05#!w$i%@zeh54v'
+SECRET_KEY = os.environ['TODO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -127,3 +128,5 @@ STATICFILES_DIRS = [
 
 
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
