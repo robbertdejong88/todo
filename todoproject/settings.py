e85542e9881d99800f9dde4d5d8b65bfd9bb333f
robbertdejong88 <55914370+robbertdejong88@users.noreply.github.com>
2020-02-19 16:52:08 +0100

    templates in settings-dirs gezet

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'todoproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'todo/templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
